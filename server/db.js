7e0cb0dd0e4da299e565c2507049ba3a348cf10c
Joel <janyanti@andrew.cmu.edu>
2019-08-12 15:59:25 -0500

    Objectify DB functions into 'DatabaseWrapper'

--- server/db.js
+++ server/db.js
@@ -5,208 +5,212 @@
 /*            Imports            */
 
 const K = require('./const')
-
 const MongoDB = require('mongodb');
 const MongoClient = MongoDB.MongoClient;
 const ObjectId = MongoDB.ObjectId
 
 /*        Database Setup         */
 
-const client = new MongoClient(K.DB_URI, { useNewUrlParser: true });
-
-/*            Functions          */
-
-
-/*
-  createUser: adds a new user to the database.
-              if user with identical credentials are found,
-              user will not be created.
-  @user: JavaScript Object containing user data
-  ex:
-  {
-    first_name : String,
-    last_name : String,
-    email : String,
-    profile_url : String,
-    roles: Array
+class DatabaseWrapper {
+  constructor(uri) {
+    this.uri = uri
+    this.client = new MongoClient(this.uri, { useNewUrlParser: true });
   }
-  @return: Bool
-*/
-// TODO: Unittest
-function createUser(user) {
-  let status = false
-  const email = user.email
 
-  client.connect(err => {
-    if (err) {
-      console.log(`[Databse Error][${err}]`)
-      return false
+  /*
+    createUser: adds a new user to the database.
+                if user with identical credentials are found,
+                user will not be created.
+    @user: JavaScript Object containing user data
+    ex:
+    {
+      first_name : String,
+      last_name : String,
+      email : String,
+      profile_url : String,
+      roles: Array
     }
-    const collection =
-      client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
+    @return: Bool
+  */
+  // TODO: Unittest
+  createUser(user) {
+    let status = false
+    const email = user.email
 
-    collection.findOne({ email: email }, (err, res) => {
+    this.client.connect(err => {
       if (err) {
-        console.log(`[Database Error][${err}`)
-      } else if (res) {
-        console.log(`[Datbase Error][${res.email} already in database]`)
-      } else {
-        collection.insertOne(user)
-          .then(res => { console.log(res); status = true })
-          .catch(err => console.log(`[Database Error][${err}`))
+        console.log(`[Databse Error][${err}]`)
+        return false
       }
-    })
-  });
-  client.close();
-  return status
-}
+      const collection =
+        this.client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
 
-/*
+      collection.findOne({ email: email }, (err, res) => {
+        if (err) {
+          console.log(`[Database Error][${err}`)
+        } else if (res) {
+          console.log(`[Datbase Error][${res.email} already in database]`)
+        } else {
+          collection.insertOne(user)
+            .then(res => { console.log(res); status = true })
+            .catch(err => console.log(`[Database Error][${err}`))
+        }
+      })
+    });
+
+    return status
+  }
+
+  /*
   addUserRole: Adds a role to a given user.
            Assumes given role does not exist for user.
   @user_id: hexstring pointing to specific user
   @role_id: hexstring pointing to specific role
   @return: Bool
 */
-// TODO: Unittest
-function addUserRole(user_id, role_id) {
-  let status = false
-  const _user_id = ObjectId.createFromHexString(user_id)
-  const _role_id = ObjectId.createFromHexString(role_id)
+  // TODO: Unittest
+  addUserRole(user_id, role_id) {
+    let status = false
+    const _user_id = ObjectId.createFromHexString(user_id)
+    const _role_id = ObjectId.createFromHexString(role_id)
 
-  client.connect(err => {
-    if (err) {
-      console.log(`[Databse Error][${err}]`)
-      return false
-    }
-    const collection =
-      client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
-
-    collection.updateOne({ _id: _user_id }, { $push: { roles: _role_id } }, (err, res) => {
+    this.client.connect(err => {
       if (err) {
-        console.log(`[Database Error][${err}`)
-      } else if (res.modifiedCount === res.matchedCount) {
-        console.log(`[Database Success][User: ${user_id} updated with Role: ${role_id}]`);
-        status = true
-      } else {
-        console.log(`[Datbase Error][ID: ${user_id} does not exist database]`)
+        console.log(`[Databse Error][${err}]`)
+        return false
       }
-    })
-  });
-  client.close();
-  return status
-}
+      const collection =
+        this.client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
 
-/*
-getUserRoles: retrieves list of roles assigned to a user.
-@user_id: hexstring pointing to specific user
-@return: Array
-*/
-// TODO: Unittest
-function getUserRoles(user_id) {
-  let ret = []
-  const _user_id = ObjectId.createFromHexString(user_id)
+      collection.updateOne({ _id: _user_id }, { $push: { roles: _role_id } }, (err, res) => {
+        if (err) {
+          console.log(`[Database Error][${err}`)
+        } else if (res.modifiedCount === res.matchedCount) {
+          console.log(`[Database Success][User: ${user_id} updated with Role: ${role_id}]`);
+          status = true
+        } else {
+          console.log(`[Datbase Error][ID: ${user_id} does not exist database]`)
+        }
+      })
+    });
 
-  client.connect(err => {
-    if (err) {
-      console.log(`[Databse Error][${err}]`)
-      return false
-    }
-    const collection =
-      client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
-
-    collection.findOne({ _id: _user_id }, (err, res) => {
-      if (err) {
-        console.log(`[Database Error][${err}`)
-      } else if (res.roles) {
-        res.roles.forEach(role_id => {
-          ret.push(K.ROLES_ID[role_id])
-        });
-        console.log(`[Datbase Success][${ret}]`)
-      } else {
-        console.log(`[Datbase Error][ID: ${user_id} does not have any roles]`)
-      }
-    })
-  });
-  client.close();
-  return ret
-}
-
-/*
-addServiceRequest: Add tutoring service request as per user (student).
-@user_id: hexstring pointing to specific user
-@request: JavaScript object containing information about service request
-ex:
-  {
-    _course_id: ObjectId
-    digest_time: Date
-    status: String
+    return status
   }
-@return: Bool
-*/
-// TODO: Unittest
-function addServiceRequest(user_id, request) {
-  let status = false
-  request._user_id = ObjectId.createFromHexString(user_id)
 
-  client.connect(err => {
-    if (err) {
-      console.log(`[Databse Error][${err}]`)
-      return false
-    }
-    const collection =
-      client.db(K.DB_OHQ).collection(K.SERVICE_REQUEST_COLLECTION);
+  /*
+  getUserRoles: retrieves list of roles assigned to a user.
+  @user_id: hexstring pointing to specific user
+  @return: Array
+  */
+  // TODO: Unittest
+  getUserRoles(user_id) {
+    let ret = []
+    const _user_id = ObjectId.createFromHexString(user_id)
 
-    collection.insertOne(request, (err, res) => {
+    this.client.connect(err => {
       if (err) {
-        console.log(`[Database Error][${err}`)
-      } else if (res.insertedCount === 1) {
-        console.log(`[Datbase Success][Added service request with ID: ${res.insertedId}]`)
-        status = true
+        console.log(`[Databse Error][${err}]`)
+        return false
       }
-    })
-  });
-  client.close();
-  return status
-}
+      const collection =
+        this.client.db(K.DB_OHQ).collection(K.USER_COLLECTION);
 
-/*
-addServiceProvider: Add tutoring service provider as per user (tutor).
-@user_id: hexstring pointing to specific user
-@request: JavaScript object containing information about service request
-ex:
-  {
-    _course_id: ObjectId
-    digest_time: Date
-    status: String
-    location: String
-    availability: { day: String , start: String, end: String }
+      collection.findOne({ _id: _user_id }, (err, res) => {
+        if (err) {
+          console.log(`[Database Error][${err}`)
+        } else if (res.roles) {
+          res.roles.forEach(role_id => {
+            ret.push(K.ROLES_ID[role_id])
+          });
+          console.log(`[Datbase Success][${ret}]`)
+        } else {
+          console.log(`[Datbase Error][ID: ${user_id} does not have any roles]`)
+        }
+      })
+    });
+
+    return ret
   }
-@return: Bool
-*/
-// TODO: Unittest
-function addServiceProvider(user_id, provider) {
-  let status = false
-  provider._user_id = ObjectId.createFromHexString(user_id)
 
-  client.connect(err => {
-    if (err) {
-      console.log(`[Databse Error][${err}]`)
-      return false
+  /*
+  addServiceRequest: Add tutoring service request as per user (student).
+  @user_id: hexstring pointing to specific user
+  @request: JavaScript object containing information about service request
+  ex:
+    {
+      _course_id: ObjectId
+      digest_time: Date
+      status: String
     }
-    const collection =
-      client.db(K.DB_OHQ).collection(K.SERVICE_PROVIDER_COLLECTION);
+  @return: Bool
+  */
+  // TODO: Unittest
+  addServiceRequest(user_id, request) {
+    let status = false
+    request._user_id = ObjectId.createFromHexString(user_id)
 
-    collection.insertOne(provider, (err, res) => {
+    this.client.connect(err => {
       if (err) {
-        console.log(`[Database Error][${err}`)
-      } else if (res.insertedCount === 1) {
-        console.log(`[Datbase Success][Added service request with ID: ${res.insertedId}]`)
-        status = true
+        console.log(`[Databse Error][${err}]`)
+        return false
       }
-    })
-  });
-  client.close();
-  return status
+      const collection =
+        this.client.db(K.DB_OHQ).collection(K.SERVICE_REQUEST_COLLECTION);
+
+      collection.insertOne(request, (err, res) => {
+        if (err) {
+          console.log(`[Database Error][${err}`)
+        } else if (res.insertedCount === 1) {
+          console.log(`[Datbase Success][Added service request with ID: ${res.insertedId}]`)
+          status = true
+        }
+      })
+    });
+
+    return status
+  }
+
+  /*
+  addServiceProvider: Add tutoring service provider as per user (tutor).
+  @user_id: hexstring pointing to specific user
+  @request: JavaScript object containing information about service request
+  ex:
+    {
+      _course_id: ObjectId
+      digest_time: Date
+      status: String
+      location: String
+      availability: { day: String , start: String, end: String }
+    }
+  @return: Bool
+  */
+  // TODO: Unittest
+  addServiceProvider(user_id, provider) {
+    let status = false
+    provider._user_id = ObjectId.createFromHexString(user_id)
+
+    this.client.connect(err => {
+      if (err) {
+        console.log(`[Databse Error][${err}]`)
+        return false
+      }
+      const collection =
+        this.client.db(K.DB_OHQ).collection(K.SERVICE_PROVIDER_COLLECTION);
+
+      collection.insertOne(provider, (err, res) => {
+        if (err) {
+          console.log(`[Database Error][${err}`)
+        } else if (res.insertedCount === 1) {
+          console.log(`[Datbase Success][Added service request with ID: ${res.insertedId}]`)
+          status = true
+        }
+      })
+    });
+
+    return status
+  }
+
 }
+
+module.exports = DatabaseWrapper;
 
--- server/db_unittest.js
+++ server/db_unittest.js
@@ -4,31 +4,35 @@
 
 /*            Imports            */
 
+const MongoDB = require('mongodb');
 const K = require('./const')
 const DB = require('./db')
+const ObjectId = MongoDB.ObjectId
 
 /*          Test Setup           */
 
-// DB.createUser({
-//   first_name : 'JP',
-//   last_name : 'Nelson',
-//   email : 'JP.Nelson@gmail.com',
-//   profile_url : 'https://www.linkedin.com/in/jp-nelson',
-//   roles: []
-// })
+var test_db = new DB(K.DB_URI)
 
-// getRoles('5d4cff134bfdf051bd1dd758')
+test_db.createUser({
+  first_name : 'JP',
+  last_name : 'Nelson',
+  email : 'JP.Nelson@gmail.com',
+  profile_url : 'https://www.linkedin.com/in/jp-nelson',
+  roles: []
+})
 
-// DB.addServiceRequest('5d4cff134bfdf051bd1dd758', {
-//   _course_id: ObjectId.createFromHexString('5d4f7529cbe5749f112b4769'),
-//   digest_time: new Date(),
-//   status: 'Pending'
-// })
+test_db.getUserRoles('5d4cff134bfdf051bd1dd758')
 
-// DB.addServiceProvider('5d4cff134bfdf051bd1dd758', {
-//   _course_id: ObjectId.createFromHexString('5d4f7529cbe5749f112b4769'),
-//   digest_time: new Date(),
-//   status: 'Pending',
-//   location: 'Wean Hall',
-//   availability: { day: 'Tuesday', start: '18:00', end: '19:30' }
-// })
+test_db.addServiceRequest('5d4cff134bfdf051bd1dd758', {
+  _course_id: ObjectId.createFromHexString('5d4f7529cbe5749f112b4769'),
+  digest_time: new Date(),
+  status: 'Pending'
+})
+
+test_db.addServiceProvider('5d4cff134bfdf051bd1dd758', {
+  _course_id: ObjectId.createFromHexString('5d4f7529cbe5749f112b4769'),
+  digest_time: new Date(),
+  status: 'Pending',
+  location: 'Wean Hall',
+  availability: { day: 'Tuesday', start: '18:00', end: '19:30' }
+})
